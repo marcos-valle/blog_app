8b23d38d92bbcaaed78adb14eff552ac1d45521d
marcos-valle <marcos.vallesn@gmail.com>
2025-01-01 17:04:17 -0300

    fix: corrição de logout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import Rails from "rails-ujs";
+import Rails from "@rails-ujs";
 Rails.start();
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .

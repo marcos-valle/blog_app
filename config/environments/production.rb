1dc84b40e958c32c641d427d03ebec3b1b500184
marcos-valle <marcos.vallesn@gmail.com>
2025-01-06 21:16:55 -0300

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,8 +51,6 @@ Rails.application.configure do
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   # config.active_job.queue_adapter = :resque
-
-  config.require_master_key = true
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
